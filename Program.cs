84f7b826ac8628a4b22e48dfecf42fb8547d139a
pokemog <matrang@hotmail.com>
2019-02-24 11:32:30 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UDPReceiver
+{
+    class Program
+    {
+        private readonly static UdpClient _udpClient = new UdpClient(6000);
+        //private readonly Action<int> m_consumer = new Action<int>();
+        private static AsyncProducerConsumerQueue<string> _asyncProducerConsumerQueue;
+        private static readonly IPEndPoint _remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        static void Main(string[] args)
+        {
+            _asyncProducerConsumerQueue = new AsyncProducerConsumerQueue<string>(ProcessUdpData);
+            Console.WriteLine("Starting Udp listening");
+            ReceivePacketsAsync();
+            Console.Read();
+        }
+
+        private static void ReceivePacketsAsync()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var dataReceived = await _udpClient.ReceiveAsync();
+                    if (dataReceived == null)
+                    {
+
+                    }
+                    var data = Encoding.UTF8.GetString(dataReceived.Buffer);
+                    var queueSize = await _asyncProducerConsumerQueue.GetQueueSize();
+                    Console.WriteLine($"Data received: {data} and adding to queue with size {queueSize}");
+                    await _asyncProducerConsumerQueue.ProduceAsync(data);
+                }
+            });
+
+        }
+
+        private static void ProcessUdpData(string udpData)
+        {
+            Task.Run(async () =>
+            {
+                Console.WriteLine($"Taking {udpData} from queue and processing");
+                Thread.Sleep(100);
+            });
+        }
+    }
+
+
+}
